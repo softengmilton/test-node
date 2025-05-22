da202b08af639f5dd5dcc52569711c2d10fc3532
Milton Dey Mishu <114005218+softengmilton@users.noreply.github.com>
2025-05-22 11:14:33 +0100

    package installed

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const router = require('./router/auth-router');
+const connectDB = require('./utils/db');
 
+
+
+app.use(express.json());
 app.use('/api/auth', router);
 
 
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+connectDB().then(() => {
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
 });
+
